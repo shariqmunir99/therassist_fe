2e46fba93154825527c510f9cadeaad1958a690e
shariqmunir99 <shariq.munir7@gmail.com>
2025-09-17 09:59:37 +0500

    initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,42 @@
+export const defaultNavItems = [
+  {
+    label: "Home",
+    url: "/",
+    isDropdown: false,
+    dropdown: [],
+  },
+  {
+    label: "Company",
+    isDropdown: true,
+    url: "",
+    dropdown: [
+      {
+        label: "Our Mission",
+        url: "/our-mission",
+        isDropdown: false,
+        dropdown: [],
+      },
+      {
+        label: "Therapist Reviews",
+        url: "/therapist-reviews",
+        isDropdown: false,
+        dropdown: [],
+      },
+    ],
+  },
+  {
+    label: "Privacy",
+    url: "/privacy",
+    isDropdown: false,
+    dropdown: [],
+  },
+  {
+    label: "Pricing",
+    url: "/pricing",
+    isDropdown: false,
+    dropdown: [],
+  },
+];
+
+export const profilePic =
+  "https://lh3.googleusercontent.com/aida-public/AB6AXuBFW1TxDfPZrZj-_ft2IP5oMDHKFwxSI97S-M0jBxQ4UPvJrTh2wiUbaY4YbFArB0Z3oWTead47Z2yCVVMKBQv3KhGfOAu5MHp-D_97j3nD9z3J-j1BV-66-wMOlu9GE6E_9scu094Ph1oTrhvVREjZ2GAgiVKvqw9jpTh0zWuGkTHs00qUxdHKKQB4PfT3mdBTnAeb3TrjBkNtWpV2J1lLx7NQyy6h42X7qgHSx1vpzHOXCBsFKB-G-mElA9TqtjCsxaHFcZCx7nk";
